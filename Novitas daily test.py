2a8dffef900a8db484d2414d96679d4aefe29965
calkinss1 <71899705+calkinss1@users.noreply.github.com>
2020-11-13 10:07:46 -0700

    Update Novitas daily test.py

--- Novitas daily test.py
+++ Novitas daily test.py
@@ -3,52 +3,99 @@ Name=input('Student Name:')
 while True:
         try:
             Meals=int(input('Number of Meals(between 0-3):'))
-        except ValueError:
-                print ('Sorry your response must be a value between 0-3')
-                continue
-        else:
-            break
-
-        while True:
             if 0<= Meals <=3:
                 break
+            raise Exception()
+        except:
             print('not an appropriate choice please select a number between 0-3')
-            #if Meals== 3:
-            #Meals= 'ate all 3 meals today'
-            #elif Meals==2:
-            #Meals='ate 2 meals today'
-            #elif Meals==1:
-            #Meals='ate only 1 meal today'
-            #elif Meals==0:
-            #Meals='did not eat any meals today'
+if Meals== 3:
+    Meals= 'ate all 3 meals today'
+elif Meals==2:
+    Meals='ate 2 meals today'
+elif Meals==1:
+    Meals='ate only 1 meal today'
+elif Meals==0:
+    Meals='did not eat any meals today'
 
-print (Meals)
-#mornteeth=input('Did student brush their teeth this morning? y or n:')
-#if mornteeth== 'y':
-    #mornteeth='he brushed his teeth this morning'
-#elif mornteeth== 'n':
-    #mornteeth='he did not brush his teeth this morning'
-#else:
-    #print(' invalid input, please enter y or n')
+while True:
+    mornteeth=input('Did student brush their teeth this morning? y or n:')
+    if  mornteeth not in ('y','n'):
+        print('not an appropriate choice please enter y or n ')
+        continue
+    else:
+        break
+if mornteeth== 'y':
+    mornteeth='he brushed his teeth this morning'
+elif mornteeth== 'n':
+    mornteeth='he did not brush his teeth this morning'
 
-#amshower=input('Did the student shower this morning? y or n:')
-#if amshower=='y':
-    #amshower='He showered this morning'
-#elif amshower=='n':
-    #amshower= input('Did the student shower this evening? y or n:' )
-    #if amshower=='y':
-        #amshower='He showered this evening'
+while True:
+    amshower=input('Did the student shower this morning? y or n:')
+    if amshower not in ('y','n'):
+        print('not and appropriate choice please enter y or n')
+        continue
+    else:
+        break
+if amshower=='y':
+    amshower='He showered this morning'
+elif amshower=='n':
+    while True:
+        amshower= input('Did the student shower this evening? y or n:' )
+        if amshower not in ('y','n'):
+            continue
+        else:
+            break
+    if amshower=='y':
+        amshower='He showered this evening'
+    if amshower=='n':
+        amshower=='He did not shower today'
 
-#school=int(input('How many School checks(0-4):'))
-#if school==4:
-    #school='He went to school and participated for the entire duration'
-#elif school==3:
-    #school='He went to school and participated for most of the day'
-#elif school==2:
-    #school='He went to school and participated for half of the schoolday'
-#elif school==1:
-    #school='He was briefly at school and minimally participated'
-#elif school==0:
-    #school='He did not go to school today'
-#print(Name,Meals+','+mornteeth+'.',amshower)
-#print(school+'.')
+while True:
+
+    bedroomclean=input('Did they clean their room today? y or n:')
+    if bedroomclean not in ('y','n'):
+        print('not and appropriate choice please enter y or n')
+        continue
+    else:
+        break
+if bedroomclean=='y':
+    bedroomclean='He cleaned his bedroom today'
+elif bedroomclean=='n':
+    bedroomclean='He did not clean his bedroom today'
+
+while True:
+        try:
+            school=int(input('How many School checks?(0-4):'))
+            if 0<= school <=4:
+                break
+            raise Exception()
+        except:
+            print('not an appropriate choice please select a number between 0-4')
+
+if school==4:
+    school='He went to school and participated for the entire duration'
+elif school==3:
+    school='He went to school and participated for most of the day'
+elif school==2:
+    school='He went to school and participated for half of the schoolday'
+elif school==1:
+    school='He was briefly at school and minimally participated'
+elif school==0:
+    school='He did not go to school today'
+
+while True:
+    largemuscle=input('Did they complete a large muscle activity for 1 hour today? y or n:')
+    if largemuscle not in ('y','n'):
+        print('not and appropriate choice please enter y or n')
+        continue
+    else:
+        break
+if largemuscle=='y':
+    largemuscle='He completed his large muscle today, for large muscle he'
+
+elif largemuscle=='n':
+    largemuscle='He did not complete a large muscle activity today'
+activity=input('What activity did they do for large muscle?:' )
+
+
+print(Name,Meals+','+mornteeth+'.',amshower+'.',bedroomclean+'.',school+'.', largemuscle,activity+'.')
